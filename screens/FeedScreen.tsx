3a4d55fd0baf57fa4f0b005eea9313090e59eff3
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2023-02-08 10:21:28 -0600

    🫣 Show/hide statusbar while scrolling

--- screens/FeedScreen.tsx
+++ screens/FeedScreen.tsx
@@ -1,12 +1,18 @@
-import {RefreshControl, ScrollView, StyleSheet, View} from 'react-native';
+import {RefreshControl, ScrollView, StyleSheet, View, Animated} from 'react-native';
 
 import { Hr } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 import React from "react";
 import LargeArticle from "../components/Article/LargeArticle";
 import Layout from "../constants/Layout";
+import {useCollapsibleHeader} from "react-navigation-collapsible";
+import {DefaultTheme, DarkTheme} from '@react-navigation/native';
+import useColorScheme from "../hooks/useColorScheme";
 
+// The docs say I need to disable the translucent status bar, but I haven't had any problems (yet)
+// https://github.com/benevbright/react-navigation-collapsible
 export default function FeedScreen({ navigation }: RootTabScreenProps<'FeedTab'>) {
+  const colorScheme = useColorScheme();
   const [refreshing, setRefreshing] = React.useState(false);
 
   const onRefresh = React.useCallback(() => {
@@ -16,9 +22,23 @@ export default function FeedScreen({ navigation }: RootTabScreenProps<'FeedTab'>
     }, 2000);
   }, []);
 
+  const options = {
+    navigationOptions: {
+      headerStyle: { backgroundColor: colorScheme === 'dark' ? DarkTheme.colors.card : DefaultTheme.colors.card },
+    },
+  };
+  const {
+    onScroll /* Event handler */,
+    containerPaddingTop /* number */,
+    scrollIndicatorInsetTop /* number */,
+  } = useCollapsibleHeader(options);
+
   // TODO: I should probably use a FlatList b/c that doesn't render all elements at once
   return (
-    <ScrollView
+    <Animated.ScrollView
+      onScroll={onScroll}
+      contentContainerStyle={{ paddingTop: containerPaddingTop }}
+      scrollIndicatorInsets={{ top: scrollIndicatorInsetTop }}
       refreshControl={
         <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
       }
@@ -37,7 +57,7 @@ export default function FeedScreen({ navigation }: RootTabScreenProps<'FeedTab'>
         <LargeArticle imgUrl="https://reactnative.dev/img/tiny_logo.png" title={"Headline 1"} date={new Date()} />
         <Hr />
       </View>
-    </ScrollView>
+    </Animated.ScrollView>
   );
 }
 
