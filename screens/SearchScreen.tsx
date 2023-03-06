12fe20f05abe7e5c37c0641a4c96fbf036453be5
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2023-03-05 23:35:22 -0500

    Revert "🛠️ Test a fix for search contents not loading"
    This reverts commit 78c4fd6f1f3a7828ea135ae72ec640f6d0670e83.

--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -1,4 +1,4 @@
-import {Button, RefreshControl, ScrollView} from 'react-native';
+import {RefreshControl, ScrollView} from 'react-native';
 import {Text} from "../components/Themed";
 import {getAllAuthors, getAllTags, search} from "../constants/api";
 import useAsync from "../hooks/useAsync";
@@ -16,7 +16,7 @@ export default function SearchScreen({route}: RootStackScreenProps<"Search">) {
   const [topics, setTopics] = useState({} as Record<string, number>);
   const [authors, setAuthors] = useState({} as Awaited<ReturnType<typeof getAllAuthors>>);
   const [tags, setTags] = useState({} as Awaited<ReturnType<typeof getAllTags>>);
-  const [pages, setPages] = useState<AsyncIterator<FullArticle>>(noop);
+  const [pages, setPages] = useState([] as (FullArticle)[]);
   const [refreshing, setRefreshing] = useState(false);
   const debouncedQuery = useDebounce<string>(query, 500);
 
@@ -27,7 +27,6 @@ export default function SearchScreen({route}: RootStackScreenProps<"Search">) {
     console.log("Searching");
     const results = search(query, domain);
 
-    /*
     setResults((async function*() {
       yield* Object.entries(await results.topics).map(([topic, id]) => (
         <SearchItem key={topic} title={topic} id={id} domain="Topics" />
@@ -42,13 +41,10 @@ export default function SearchScreen({route}: RootStackScreenProps<"Search">) {
         yield <SmallArticle data={page} key={page.id} />;
     })());
     return;
-    */
 
     results.topics.then(setTopics);
     results.authors.then(setAuthors);
     results.tags.then(setTags);
-    setPages(results.posts);
-    return;
     Promise.all(Object.values(results)).then(() => setRefreshing(false));
 
     // I think it is safe to ignore promise rejection: next() is undefined
@@ -64,23 +60,13 @@ export default function SearchScreen({route}: RootStackScreenProps<"Search">) {
     pages.then(setPages);
   }, [debouncedQuery, domain]);
   console.log("rerendering search screen");
-  pages.next().then(result => console.log("Has results?", !result.done));
+  results.next().then(result => console.log(result.value));
 
   return (
     <InfiniteScroll
-      iterator={pages}
-      renderItem={({item: page}) => <SmallArticle data={page} key={page.id} />}
-    >
-      {Object.entries(topics).map(([topic, id]) => (
-        <SearchItem key={topic} title={topic} id={id} domain="Topics" />
-      ))}
-      {Object.entries(authors).map(([author, deets]) => (
-        <SearchItem key={author} title={author} id={deets.id} img={deets.avatar_urls?.["96"]} domain="Authors" />
-      ))}
-      {Object.entries(tags).map(([tag, deets]) => (
-        <SearchItem key={tag} title={tag} id={deets.id} domain="Tags" />
-      ))}
-    </InfiniteScroll>
+      iterator={results}
+      renderItem={thing => <>{thing}</>}
+    />
   );
 }
 
