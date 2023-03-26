84ae84586097e7cb75a97fbbb88df215e90c919c
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2023-03-26 18:03:21 -0500

    Modify blank search to use cards, as requested

--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -1,4 +1,4 @@
-import {search, searchDomains} from "../constants/api";
+import wp, {getAllCategories, search, searchDomains} from "../constants/api";
 import {RootStackScreenProps, RootTabScreenProps} from "../types";
 import SmallArticle from "../components/Article/SmallArticle";
 import React, {useEffect, useState} from "react";
@@ -6,15 +6,19 @@ import useDebounce from "../hooks/useDebounce";
 import SearchItem from "../components/SearchItem";
 import InfiniteScroll from "../components/InfiniteScroll";
 import SegmentedSearch from "../components/SegmentedSearch";
-import {Text} from "../components/Themed";
+import {Text, Title, useAndroidRipple} from "../components/Themed";
 import useColorScheme from "../hooks/useColorScheme";
 import Colors from "../constants/Colors";
+import useAsyncIterator from "../hooks/useAsyncIterator";
+import useAsync from "../hooks/useAsync";
+import {Dimensions, Pressable, View} from "react-native";
+import Layout from "../constants/Layout";
+import {useNavigation} from "@react-navigation/native";
 
 export default function SearchScreen({route, navigation}: RootStackScreenProps<"Search"> | RootTabScreenProps<"SearchTab">) {
   const {query = "", domain = "All", addNotifications} = route.params ?? {};
   const debouncedQuery = useDebounce<string>(query, 500);
   const [results, setResults] = useState<AsyncIterator<JSX.Element>>(noop);
-  const colors = Colors[useColorScheme()];
 
   useEffect(() => {
     navigation.setOptions({
@@ -23,11 +27,7 @@ export default function SearchScreen({route, navigation}: RootStackScreenProps<"
   }, []);
 
   useEffect(() => {
-    // This shows all tags if query="" & domain=All
     let domain = route.params?.domain ?? "All";
-    if (query.length === 0 && domain === "All")
-      domain = "Topics";
-
     if (query.length === 0 && (domain === "All" || domain === "Posts")) {
       setResults(noop())
       return;
@@ -54,9 +54,44 @@ export default function SearchScreen({route, navigation}: RootStackScreenProps<"
     <InfiniteScroll
       iterator={results}
       renderItem={({item}) => item}
-      ListEmptyComponent={() => <Text style={{color: colors.shadow, textAlign: "center", padding: 10}}>No results... yet!</Text>}
+      // @ts-ignore expected Element, got Element[] (it still works)
+      ListEmptyComponent={ListEmptyComponent.bind(null, route.params ?? {})}
     />
   );
+}
+
+function ListEmptyComponent(props: Parameters<typeof SearchScreen>[0]["route"]["params"]) {
+  const {query = "", domain = "All"} = props;
+  const colorScheme = useColorScheme();
+  const colors = Colors[colorScheme];
+  const ripple = useAndroidRipple();
+  const navigation = useNavigation();
+  const topics = useAsync(getAllCategories(wp.categories().perPage(100)), {});
+  const padding = 30;
+
+  if (query.length === 0 && domain === "All") {
+    return <View style={{padding, alignItems: "center"}}>{
+      Object.entries(topics).map(([topic, id], index, entries) => (
+        <Pressable
+          android_ripple={ripple}
+          style={{
+            width: Math.min(360, Dimensions.get('window').width - padding),
+            height: 100,
+            borderRadius: 10,
+            marginBottom: padding,
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: `hsl(${index * 360 / entries.length}, 80%, ${colorScheme === "dark" ? 50 : 60}%)`,
+          }}
+          onPress={() => navigation.navigate("SearchDetails", {domain: "Topics", id, title: topic})}
+        >
+          <Title>{topic}</Title>
+        </Pressable>
+      ))
+    }</View>
+  } else {
+    return <Text style={{color: colors.shadow, textAlign: "center", padding: 10}}>No results... yet!</Text>;
+  }
 }
 
 async function* noop() {
