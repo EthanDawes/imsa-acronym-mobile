f751ce99f5eafbc81d4ca7d03d6daffe563ad08c
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2023-05-11 23:27:45 -0500

    increase LargeArticle gap

--- components/Article/LargeArticle.tsx
+++ components/Article/LargeArticle.tsx
@@ -4,18 +4,21 @@ import ArticleContainer from "./ArticleContainer";
 import ArticleImage from "./ArticleImage";
 import useColorScheme from "../../hooks/useColorScheme";
 import Colors from "../../constants/Colors";
+import {View} from "react-native";
 
 
 export default function LargeArticle(props: ArticleProps) {
   const {imgUrl, title, excerpt, categories, author} = props.data;
   return (
     <ArticleContainer {...props}>
-      <ArticleImage src={imgUrl} />
-      <CategoryLabel style={{marginTop: 10}}>{Object.keys(categories).join(", ")}</CategoryLabel>
-      <Title>{title}</Title>
-      {/*TODO: add a little picture?*/}
-      <Italics>{author.name}</Italics>
-      <Text>{excerpt}</Text>
+      <View style={{gap: 7}}>
+        <ArticleImage src={imgUrl} />
+        <CategoryLabel style={{marginTop: 10}}>{Object.keys(categories).join(", ")}</CategoryLabel>
+        <Title>{title}</Title>
+        {/*TODO: add a little picture?*/}
+        <Italics>{author.name}</Italics>
+        <Text>{excerpt}</Text>
+      </View>
     </ArticleContainer>
   );
 }
